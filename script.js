027770e9493d72baca125d7daa63ae1d80aa889b
ahmadtufail1158 <ahmadtufail1158490@gmail.com>
2024-06-16 19:15:51 +0500

    "done"

---
+++ script.js
@@ -0,0 +1,71 @@
+ 
+ 
+// let colors = ["Red","Blue","Green","Purpal","White"]
+// console.log(colors.indexOf("Green"));
+
+
+
+
+
+
+// let colors = [];
+// colors[0] = "Red";
+// colors[1] = "Blue";
+// colors[2] = "Green";
+// colors[3] = "Purpal";
+// colors[4] = "White";
+
+// console.log(colors[3]);
+
+
+
+
+
+// let colors = [];
+// colors[0] = "Red";
+// colors[1] = "Blue";
+// colors[2] = "Green";
+// colors[3] = "Purpal";
+// colors[4] = "White";
+
+// colors.pop();
+// colors.push("Black","Grey");
+
+// console.log(colors);
+
+
+
+
+// let scores = [10, 20, 30, 40, 50];
+//  scores[3] = 25;
+
+//  console.log(scores);
+
+
+
+
+// let arr2 = [1,2,3,4,5]
+// let arr3 = [6,7,8,9,10]
+
+// let arr4 = arr2.concat(arr3);
+
+// console.log(arr4);
+
+
+
+
+
+// let colors = ["Red","Blue","Green","Purpal"]
+
+//   colors.shift();
+//  console.log(colors);
+
+
+
+
+
+//  let colors = ["Red","Blue","Green","Purpal"]
+
+//   colors.unshift("Black","White","22245");
+//   console.log(colors);
+
